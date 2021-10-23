a6f2e9c453477f5db87afd8ad6c3fe8b1b7a94e2
Mark Baggett <mbagget1@utk.edu>
2021-10-23 09:14:56 -0400

    Initialize ActivitiesPage.

--- change_discovery/change_discovery.py
+++ change_discovery/change_discovery.py
@@ -16,6 +16,14 @@ class ChangeDiscoveryRequest:
             raise KeyError(f"{self.type} is missing a type value.")
         return
 
+    def validate_id(self):
+        if "id" in self.details:
+            if not self.details["id"].startswith("https"):
+                raise ValueError(f"{self.type} id does not start with https.")
+        else:
+            raise KeyError(f"{self.type} missing id.")
+        return
+
 
 class OrderedCollection(ChangeDiscoveryRequest):
     """A class to represent an ActivityStreams Collection."""
@@ -27,20 +35,24 @@ class OrderedCollection(ChangeDiscoveryRequest):
         self.only_delete = False
 
     def __validate(self):
-        self.__validate_id()
+        self.validate_id()
         self.validate_type()
         if "last" not in self.details:
             raise KeyError("OrderedCollection missing required last property.")
         return
 
-    def __validate_id(self):
-        if "id" in self.details:
-            if not self.details["id"].startswith("https"):
-                raise ValueError("OrderedCollection id does not start with https.")
-        else:
-            raise KeyError("OrderedCollection missing id.")
+
+class ActivitiesPage(ChangeDiscoveryRequest):
+    """A Class to represent an ActivityStreams CollectionPage"""
+    def __init__(self, url, last_crawl=""):
+        super().__init__(url, "OrderedCollectionPage")
+        self.__validate()
+
+    def __validate(self):
+        self.validate_id()
+        self.validate_type()
         return
 
 
 if __name__ == "__main__":
-    OrderedCollection("https://iiif.bodleian.ox.ac.uk/iiif/activity/page-0")
+    OrderedCollection("https://iiif.bodleian.ox.ac.uk/iiif/activity/page-171")
